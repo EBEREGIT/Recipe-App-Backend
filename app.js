8f78d64fbde09161c5ac34ce12636eea1cdec07c
EBEREGIT <samsoneberenjoku@gmail.com>
2019-09-27 13:19:04 +0100

    connected the app to MongoDB with mongoose

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+// call and initialize express here
 const app = express();
 
+// connect this app to MongoDB with mongoose
+mongoose.connect('mongodb+srv://Ebere:31904145@cluster0-ev2sa.mongodb.net/test?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas, Yaeyeh!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+  
 // Curb Cores Error by adding a header here
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -10,9 +23,20 @@ app.use((req, res, next) => {
     next();
   });
 
+//   to help extract .json object use the body parser as follows
+  app.use(bodyParser.json());
+
 //   check for successfull server connection here
 app.use((req, res) => {
    res.json({ message: 'Your request was successfully successful!' }); 
 });
+
+// Create an item here
+app.post('/api/recipes', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Recipe created successfully!'
+    });
+  });
 
 module.exports = app;
